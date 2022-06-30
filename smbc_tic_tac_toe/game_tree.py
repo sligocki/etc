50b329254b153445ef8e3d1db82eb8c07ed10353
Shawn Ligocki <sligocki@gmail.com>
2022-06-30 15:14:11 -0400

    Add Tree.walk_back() to walk nodes backwards. This will be useful for many things (like min-max).

--- smbc_tic_tac_toe/game_tree.py
+++ smbc_tic_tac_toe/game_tree.py
@@ -29,24 +29,44 @@ class Tree:
   def num_positions(self):
     return len(self.children)
 
-  def num_games(self, pos = None):
-    if not pos:
-      pos = self.init_pos
-    if not self.children[pos]:
+  def _walk_help(self, visited, pos):
+    if pos not in visited:
+      visited.add(pos)
+      # 1) Yield all descendants
+      for next_pos in self.children[pos]:
+        for p in self._walk_help(visited, next_pos):
+          yield p
+      # 2) Yield self
+      yield pos
+
+  def walk_back(self):
+    """Enumerate all postitions in the game tree backwards, starting from leaf
+    nodes. You are guaranteed that you will only see a position after all of
+    it's children."""
+    visited = set()
+    for pos in self._walk_help(visited, self.init_pos):
+      yield pos, self.children[pos]
+
+
+def num_games(tree):
+  num_subgames = {}
+  for pos, children in tree.walk_back():
+    if not children:
       # If there are no following moves, this is a single completed game trace.
-      return 1
+      num_subgames[pos] = 1
     else:
       # Otherwise, add up all the following counts.
       # Note: This is inefficient because it's not memoized ... but I think it
       # doesn't matter for tic-tac-toe b/c it's small enough ...
-      return sum(self.num_games(next_pos) for next_pos in self.children[pos])
+      num_subgames[pos] = sum(num_subgames[next_pos] for next_pos in children)
+  return num_subgames[tree.init_pos]
 
 
 def test():
   tree = Tree()
   tree.build()
   print("Number of pos_to_next:", tree.num_positions())
-  print("Number of games:", tree.num_games())
+  print("Number of games:", num_games(tree))
 
 
 if __name__ == "__main__":

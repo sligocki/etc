ff1ca7c37acac14216d3f26a38afaaf843a581b5
Shawn Ligocki <sligocki@gmail.com>
2022-07-09 02:41:38 -0400

    * Normalize direction (always > before <). * Add bf_enum option to list all progs of a given length.

--- brainfuck_busy_beaver/bf_enum.py
+++ brainfuck_busy_beaver/bf_enum.py
@@ -53,30 +53,52 @@ def _bf_enum_opt_help(size, *, allow_end_loop=True, only_end_loop=False):
         for i in simple_instr_opt(prefix):
           yield prefix + i
 
+def is_dir_norm(prog):
+  """Return true if program does not have a < before a >"""
+  r = prog.find(">")
+  l = prog.find("<")
+  if l == -1:
+    # No < : Good
+    return True
+  if r == -1:
+    # Has < but no > : Bad
+    return False
+  # Has both < and > : Does > come first?
+  return r < l
+
 def bf_enum_opt(size):
-  # Optimization: Always end with ]. Any other ending is inefficient.
-  # At least for num_steps, you could get the same value by starting with an equal number of >s.
-  # For score, the obvious counterexample here is that +++ is max for small sizes ...
-  # But even for score, I think there are more efficient ways to compute this in post-processing.
-  for suffix in _bf_enum_opt_help(size - 1, only_end_loop = True):
-    # Optimization: Always start with +. Any other starting symbol is inefficient.
-    # At least for num_steps, - is symmetric with +. >< are a waste and [ will always fail first.
-    yield "+" + suffix
+  if size >= 4:
+    # Optimization: Always end with ]. Any other ending is inefficient.
+    # At least for num_steps, you could get the same value by starting with an equal number of >s.
+    # For score, the obvious counterexample here is that +++ is max for small sizes ...
+    # But even for score, I think there are more efficient ways to compute this in post-processing.
+    for suffix in _bf_enum_opt_help(size - 1, only_end_loop = True):
+      # Optimization: Always start with +. Any other starting symbol is inefficient.
+      # At least for num_steps, - is symmetric with +. >< are a waste and [ will always fail first.
+      # Optimization: No +- (see kExcludePairs comment).
+      if suffix[0] != "-" and is_dir_norm(suffix):
+        yield "+" + suffix
 
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--max-size", type=int, default=10)
+  parser.add_argument("--enum", type=int)
+  parser.add_argument("--max-size", type=int)
   args = parser.parse_args()
 
-  for size in range(args.max_size + 1):
-    total_count = sum(1 for prog in bf_enum(size))
-    opt_count = sum(1 for prog in bf_enum_opt(size))
-    print(f"Size {size:4d} / Total {total_count:11_d} / Opt {opt_count:11_d}")
+  if args.enum:
+    for prog in bf_enum_opt(args.enum):
+      print(prog)
 
-  for size in range(args.max_size + 1, args.max_size + 5):
-    opt_count = sum(1 for prog in bf_enum_opt(size))
-    print(f"Size {size:4d} / Total {'N/A':11s} / Opt {opt_count:11_d}")
+  if args.max_size:
+    for size in range(args.max_size + 1):
+      total_count = sum(1 for prog in bf_enum(size))
+      opt_count = sum(1 for prog in bf_enum_opt(size))
+      print(f"Size {size:4d} / Total {total_count:11_d} / Opt {opt_count:11_d}")
+
+    for size in range(args.max_size + 1, args.max_size + 5):
+      opt_count = sum(1 for prog in bf_enum_opt(size))
+      print(f"Size {size:4d} / Total {'N/A':11s} / Opt {opt_count:11_d}")
 
 if __name__ == "__main__":
   main()

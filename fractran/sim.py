8f051bb1128fc34ab6dd6698c1b3358e64dc0aab
Shawn Ligocki <sligocki@gmail.com>
2025-11-11 23:37:46 -0500

    Share the rule number used at each step and allow filtering which steps are printed based on it.

--- fractran/sim.py
+++ fractran/sim.py
@@ -5,43 +5,46 @@ import numpy as np
 from base import State, Program
 from parse import parse_program, prime_factor
 
-def step(prog: Program, state: State) -> State | None:
-  for rule in prog:
+def step(prog: Program, state: State) -> tuple[State | None, int]:
+  for rule_num, rule in enumerate(prog):
     new_state = state + rule
     if np.all(new_state >= 0):
-      return new_state
-  return None
+      return (new_state, rule_num)
+  return (None, len(prog))
 
 def run(prog: Program, state: State, num_steps: int) -> State | None:
   cur = state
   while cur is not None and num_steps > 0:
-    cur = step(prog, cur)
+    cur, _ = step(prog, cur)
     num_steps -= 1
   return cur
 
 
-def sim(prog: Program, state: State) -> None:
+def sim(prog: Program, state: State, print_rule: int) -> None:
   print("Program:", prog)
   print("Start:", state)
 
   num_steps = 0
   while state is not None:
-    state = step(prog, state)
+    next_state, rule_num = step(prog, state)
     num_steps += 1
-    print(num_steps, state)
+    if rule_num >= print_rule:
+      print(f"{num_steps:7_d}:  r{rule_num}:  {state}  ->  {next_state}")
+    state = next_state
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("program")
-  parser.add_argument("start", type=int)
+  parser.add_argument("print_rule", type=int, nargs="?", default=0)
+  parser.add_argument("--start", type=int, default=2)
   args = parser.parse_args()
 
   prog = parse_program(args.program)
   start = prime_factor(args.start)
   start.resize(prog[0].size)
 
-  sim(prog, start)
+  sim(prog, start, args.print_rule)
 
 if __name__ == "__main__":
   main()

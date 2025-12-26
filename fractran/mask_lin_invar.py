aee8e98f1aaa929033129c5ea33cd8c1effd486a
Shawn Ligocki <sligocki@gmail.com>
2025-12-26 14:05:02 -0500

    Refactor main Z3 loop into its own function.

--- fractran/mask_lin_invar.py
+++ fractran/mask_lin_invar.py
@@ -29,6 +29,73 @@ def dot(xs, ys):
     assert len(xs) == len(ys), (xs, ys)
     return z3.Sum([v * c for v, c in zip(xs, ys)])
 
+def decide_specific(prog: Program, start_state: State, safe_indices, p_idx: int, v_idx: int, gate_var: int) -> DecideResult:
+    num_rules = prog.num_rules()
+    num_vars = prog.num_registers()
+    requirements = [min_vals(trans) for trans in prog.rules]
+
+    s = z3.Solver()
+    S1 = [z3.Int(f"S1_{i}") for i in range(num_vars)]
+    S2 = [z3.Int(f"S2_{i}") for i in range(num_vars)]
+
+    # S1 Constraints (The Floor)
+    # S1 never decreases and starts >= 1
+    # Therefore, at any point in the future it will have value >= 1
+    s.add(dot(S1, start_state.array) >= 1)
+    for i in range(num_rules):
+        s.add(dot(S1, prog.rules[i].array) >= 0) # S1 never decreases
+
+    # S2 Constraints (The Ceiling)
+    # S2 is also always >= 1, but the reasoning is slightly more complicated.
+    # It starts >= 1 and does not decrease except for one (violator) rule.
+    # After that violator rule applies, we show that S2 >= S1 >= 1.
+    s.add(dot(S2, start_state.array) >= 1)
+    # S2 is non-decreasing on all rules except the violator rule.
+    for i in range(num_rules):
+        if i != v_idx:  # No constraint on the "violator" rule
+            s.add(dot(S2, prog.rules[i].array) >= 0) # Others non-decreasing
+
+    # When S2 > 0, it is impossible for the program to halt because it must
+    # have positive values in some "safe" registers that guarantee that a
+    # rule will always apply.
+    # We guarantee this by requiring all weights to be <= 0 for unsafe registers
+    # which means that if S2 > 0, at least one safe register must be positive.
+    for i in range(num_vars):
+        if i not in safe_indices:
+            s.add(S2[i] <= 0)
+
+    # Constraints for when violator rule applies.
+    # The goal here is to prove that after the violator rule applies, S2 >= S1.
+    # In order to prove this we require:
+    #   A) S2 >= S1 before the violator rule applies (in other words, all components
+    #      of S2 >= S1 except for the gate_var (which will be 0 when violator gate applies)).
+    #   B) S2 . x >= S1 . x where x = requiremens[v] +
+
+    # The Gating Constraint (S2 must stay >= S1 after the drop)
+    # We enforce S2 >= S1 on all vars except the gate_var (which is 0).
+    for i in range(num_vars):
+        if i != gate_var:
+            s.add(S2[i] >= S1[i])
+
+    # We require that S2 >= S1 directly after applying the violator rule.
+    # Here we consider the min state after (`min_after`).
+    # If it is larger in any register other than `gate_var` (which is 0)
+    # then the above check ensures that will only make S2 - S1 bigger.
+    min_before = requirements[v_idx]
+    min_after = [min_before[i] + prog.rules[v_idx].array[i]
+                    for i in range(num_vars)]
+    S1_after = dot(S1, min_after)
+    S2_after = dot(S2, min_after)
+    s.add(S2_after >= S1_after)
+
+    if s.check() == z3.sat:
+        m = s.model()
+        S1 = [m[v].as_long() for v in S1]
+        S2 = [m[v].as_long() for v in S2]
+        return DecideResult(True, [S1, S2], num_rules, p_idx, v_idx, gate_var)
+    else:
+        return FAILED_RESULT
+
 def decide_full(prog: Program, start_state: State) -> DecideResult:
     """Decide using full program (no prefixes)"""
     num_rules = prog.num_rules()
@@ -71,65 +138,9 @@ def decide_full(prog: Program, start_state: State) -> DecideResult:
                 continue
             # If we made it to this point, then we have found a valid pair of protector and violator rules.
 
-            s = z3.Solver()
-            S1 = [z3.Int(f"S1_{i}") for i in range(num_vars)]
-            S2 = [z3.Int(f"S2_{i}") for i in range(num_vars)]
-
-            # S1 Constraints (The Floor)
-            # S1 never decreases and starts >= 1
-            # Therefore, at any point in the future it will have value >= 1
-            s.add(dot(S1, start_state.array) >= 1)
-            for i in range(num_rules):
-                s.add(dot(S1, prog.rules[i].array) >= 0) # S1 never decreases
-
-            # S2 Constraints (The Ceiling)
-            # S2 is also always >= 1, but the reasoning is slightly more complicated.
-            # It starts >= 1 and does not decrease except for one (violator) rule.
-            # After that violator rule applies, we show that S2 >= S1 >= 1.
-            s.add(dot(S2, start_state.array) >= 1)
-            # S2 is non-decreasing on all rules except the violator rule.
-            for i in range(num_rules):
-                if i != v_idx:  # No constraint on the "violator" rule
-                    s.add(dot(S2, prog.rules[i].array) >= 0) # Others non-decreasing
-
-            # When S2 > 0, it is impossible for the program to halt because it must
-            # have positive values in some "safe" registers that guarantee that a
-            # rule will always apply.
-            # We guarantee this by requiring all weights to be <= 0 for unsafe registers
-            # which means that if S2 > 0, at least one safe register must be positive.
-            for i in range(num_vars):
-                if i not in safe_indices:
-                    s.add(S2[i] <= 0)
-
-            # Constraints for when violator rule applies.
-            # The goal here is to prove that after the violator rule applies, S2 >= S1.
-            # In order to prove this we require:
-            #   A) S2 >= S1 before the violator rule applies (in other words, all components
-            #      of S2 >= S1 except for the gate_var (which will be 0 when violator gate applies)).
-            #   B) S2 . x >= S1 . x where x = requiremens[v] +
-
-            # The Gating Constraint (S2 must stay >= S1 after the drop)
-            # We enforce S2 >= S1 on all vars except the gate_var (which is 0).
-            for i in range(num_vars):
-                if i != gate_var:
-                    s.add(S2[i] >= S1[i])
-
-            # We require that S2 >= S1 directly after applying the violator rule.
-            # Here we consider the min state after (`min_after`).
-            # If it is larger in any register other than `gate_var` (which is 0)
-            # then the above check ensures that will only make S2 - S1 bigger.
-            min_before = requirements[v_idx]
-            min_after = [min_before[i] + prog.rules[v_idx].array[i]
-                         for i in range(num_vars)]
-            S1_after = dot(S1, min_after)
-            S2_after = dot(S2, min_after)
-            s.add(S2_after >= S1_after)
-
-            if s.check() == z3.sat:
-                m = s.model()
-                S1 = [m[v].as_long() for v in S1]
-                S2 = [m[v].as_long() for v in S2]
-                return DecideResult(True, [S1, S2], num_rules, p_idx, v_idx, gate_var)
+            res = decide_specific(prog, start_state, safe_indices, p_idx, v_idx, gate_var)
+            if res.infinite:
+                return res
 
     return FAILED_RESULT
 

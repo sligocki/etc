3cb1dae8f93c90c5b71d519c6fb2b590d171c26a
Shawn Ligocki <sligocki@gmail.com>
2026-04-15 18:01:01 -0400

    Rework comments in example_ack

--- gen_rec/src/example_ack.rs
+++ gen_rec/src/example_ack.rs
@@ -1,14 +1,18 @@
 /// A hand-built GRF encoding of a function with Ackermann growth.
 use crate::grf::Grf;
 
-/// Pred: x ∸ 1
+// List <-> Integer encoding:
+//    We encode a list of Nat into a Nat by using run lengths of 1s in binary encoding.
+//    Ex: [1,2,3] -> 0b10110111 = 183
+
+/// Pred(x) := x ∸ 1
 /// R(Z0, P(2,1))
 /// Arity: 1, Size: 3
 pub fn pred() -> Grf {
     Grf::Rec(Box::new(Grf::Zero(0)), Box::new(Grf::Proj(2, 1)))
 }
 
-/// Not: 1 if x = 0, else 0
+/// Not(x) := 1 if x = 0, else 0
 /// R(C(S, Z0), Z2)
 /// Arity: 1, Size: 5
 pub fn not() -> Grf {
@@ -18,7 +22,7 @@ pub fn not() -> Grf {
     )
 }
 
-/// Sgn: 0 if x = 0, else 1
+/// Sgn(x) := 0 if x = 0, else 1
 /// R(Z0, C(S, Z2))
 /// Arity: 1, Size: 5
 pub fn sgn() -> Grf {
@@ -28,14 +32,14 @@ pub fn sgn() -> Grf {
     )
 }
 
-/// Plus2: x + 2
+/// Plus2(x) := x + 2
 /// C(S, S)
 /// Arity: 1, Size: 3
 pub fn plus2() -> Grf {
     Grf::comp(Grf::Succ, vec![Grf::Succ])
 }
 
-/// Double: 2x
+/// Double(x) := 2x
 /// R(Z0, C(Plus2, P(2,2)))
 /// Arity: 1, Size: 7
 pub fn double() -> Grf {
@@ -45,7 +49,7 @@ pub fn double() -> Grf {
     )
 }
 
-/// RMonus: y ∸ x  (reversed monus; first arg is subtracted from second)
+/// RMonus(x,y) := y ∸ x
 /// R(P(1,1), C(Pred, P(3,2)))
 /// Arity: 2, Size: 7
 pub fn rmonus() -> Grf {
@@ -55,7 +59,7 @@ pub fn rmonus() -> Grf {
     )
 }
 
-/// Mod2: x mod 2
+/// Mod2(x) := x mod 2
 /// R(Z0, C(Not, P(2,2)))
 /// Arity: 1, Size: 9
 pub fn mod2() -> Grf {
@@ -65,7 +69,7 @@ pub fn mod2() -> Grf {
     )
 }
 
-/// Shift: x · 2^k  (first arg k is the shift amount, second arg x is the value)
+/// Shift(k,x) := x · 2^k
 /// R(P(1,1), C(Double, P(3,2)))
 /// Arity: 2, Size: 11
 pub fn shift() -> Grf {
@@ -75,7 +79,7 @@ pub fn shift() -> Grf {
     )
 }
 
-/// RMonusOdd: y ∸ (2x + 1)  (first arg x, second arg y)
+/// RMonusOdd(x,y) := y ∸ (2x + 1)
 /// R(Pred, C(Pred, C(Pred, P(3,2))))
 /// Arity: 2, Size: 13
 pub fn rmonus_odd() -> Grf {
@@ -85,14 +89,14 @@ pub fn rmonus_odd() -> Grf {
     )
 }
 
-/// Div2: ⌊x / 2⌋
+/// Div2(x) := ⌊x / 2⌋
 /// M(RMonusOdd)
 /// Arity: 1, Size: 14
 pub fn div2() -> Grf {
     Grf::Min(Box::new(rmonus_odd()))
 }
 
-/// Div2k: ⌊x / 2^k⌋  (first arg k, second arg x)
+/// Div2k(k,x) := ⌊x / 2^k⌋
 /// R(P(1,1), C(Div2, P(3,2)))
 /// Arity: 2, Size: 18
 pub fn div2k() -> Grf {
@@ -102,7 +106,9 @@ pub fn div2k() -> Grf {
     )
 }
 
-/// SafeBlock: x · 2^k ∸ Sgn(k)  (first arg k, second arg x)
+/// SafeBlock(k,x) := x · 2^k ∸ Sgn(k)
+///   If k ≥ 1: Append (k-1) to list x
+///   if k = 0: Do nothing
 /// C(RMonus, C(Sgn, P(2,1)), Shift)
 /// Arity: 2, Size: 26
 pub fn safe_block() -> Grf {
@@ -115,24 +121,22 @@ pub fn safe_block() -> Grf {
     )
 }
 
-/// Bit: the k-th bit of x  (first arg k, second arg x)
+/// Bit(k,x) := the k-th bit of x
 /// C(Mod2, Div2k)
 /// Arity: 2, Size: 28
 pub fn bit() -> Grf {
     Grf::comp(mod2(), vec![div2k()])
 }
 
-/// PopK: length of the lowest run of 1-bits  (= min k s.t. bit k of x = 0)
+/// PopK(x) := Last value in list x (0 for empty list)
+///   Length of the lowest run of 1-bits  (= min k s.t. bit k of x = 0)
 /// M(Bit)
 /// Arity: 1, Size: 29
 pub fn pop_k() -> Grf {
     Grf::Min(Box::new(bit()))
 }
 
-/// AppendN: apply the SafeBlock transform N times starting from x
-///   AppendN(0, k, x)   = x          [P(2,2) selects x as the 2nd rest-arg]
-///   AppendN(n+1, k, x) = SafeBlock(AppendN(n, k, x), k)
-///                       = k * 2^AppendN(n,k,x) ∸ Sgn(AppendN(n,k,x))
+/// AppendN(n, k, x): Append [k-1]*n to list x
 /// R(P(2,2), C(SafeBlock, P(4,2), P(4,3)))
 /// Arity: 3, Size: 31
 pub fn append_n() -> Grf {
@@ -145,8 +149,10 @@ pub fn append_n() -> Grf {
     )
 }
 
-/// AckStep: full state mutation
-///   AckStep(a, b) = Div2(AppendN(a, b, PopK(b)))
+/// AckStep(n, x): Apply one step of Ackermann worm to list x
+///   Equivalent to:
+///     * Pop last element of list x -> k
+///     * Append n+1 copies of k-1 to list x
 /// C(Div2, C(AppendN, P(2,1), P(2,2), C(PopK, P(2,2))))
 /// Arity: 2, Size: 80
 pub fn ack_step() -> Grf {
@@ -163,12 +169,11 @@ pub fn ack_step() -> Grf {
     )
 }
 
-/// StateLoop: iterate AckStep n times
-///   StateLoop(0, x)   = x
-///   StateLoop(n+1, x) = AckStep(n, StateLoop(n, x))
+/// AckLoop(m, x): Iterate AckStep m times on list x (with increasing values of n)
+///   It is known that for large enough m this will lead to x -> [] = 0
 /// R(P(1,1), C(AckStep, P(3,1), P(3,2)))
 /// Arity: 2, Size: 85
-pub fn state_loop() -> Grf {
+pub fn ack_loop() -> Grf {
     Grf::Rec(
         Box::new(Grf::Proj(1, 1)),
         Box::new(Grf::comp(
@@ -178,11 +183,29 @@ pub fn state_loop() -> Grf {
     )
 }
 
-/// AckWorm: A version of Hydra game/Goodstein sequence
+/// AckWorm(x): A version of Hydra game/Goodstein sequence
+///   Returns smallest m such that AckLoop(m, x) = []
 /// Arity: 1, Size: 86
 pub fn ack_worm() -> Grf {
-    Grf::Min(Box::new(state_loop()))
+    Grf::Min(Box::new(ack_loop()))
 }
+
+// TODO
+// /// ack(n) dominates any PRF
+// /// Arity: 0, Size: 
+// pub fn ack() -> Grf {
+//     Grf::comp(ack_worm(), )
+// }
+
+// pub fn rep_ack() -> Grf {
+
+// }
+
+// /// A GRF that computes a number > Graham's number
+// /// Arity: 0, Size: ?
+// pub fn graham() -> Grf {
+
+// }
 
 #[cfg(test)]
 mod tests {
@@ -215,7 +238,7 @@ mod tests {
             ("pop_k",      &pop_k,      1, 29),
             ("append_n",   &append_n,   3, 31),
             ("ack_step",   &ack_step,   2, 80),
-            ("state_loop", &state_loop, 2, 85),
+            ("ack_loop", &ack_loop, 2, 85),
             ("ack_worm",   &ack_worm,   1, 86),
         ];
         for (name, f, arity, size) in cases {
@@ -416,10 +439,10 @@ mod tests {
     }
 
     #[test]
-    fn test_state_loop() {
-        // state_loop(0, x) = x
-        // state_loop(n+1, x) = ack_step(n, state_loop(n, x))
-        let f = state_loop();
+    fn test_ack_loop() {
+        // ack_loop(0, x) = x
+        // ack_loop(n+1, x) = ack_step(n, ack_loop(n, x))
+        let f = ack_loop();
         for x in 0u64..=5 {
             assert_eq!(eval(&f, &[0, x]), Some(x));
         }
@@ -429,7 +452,7 @@ mod tests {
 
     #[test]
     fn test_ack_worm_small() {
-        // ack_worm(x) = min{n : state_loop(n,x) = 0}
+        // ack_worm(x) = min{n : ack_loop(n,x) = 0}
         let f = ack_worm();
         assert_eq!(eval(&f, &[0]),  Some(0)); // StateLoop(0,0)=0 immediately
         assert_eq!(eval(&f, &[1]),  Some(1)); // StateLoop(1,1)=ack_step(0,1)=0

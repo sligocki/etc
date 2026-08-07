9857893d37a7c6a4250bdb4d2b4138edbe0a86f9
Shawn Ligocki <sligocki@gmail.com>
2026-08-06 22:32:50 -0400

    Prove MPRF 13 Holdout 0.

--- gen_rec/proofs/GenRec/Holdouts13.lean
+++ gen_rec/proofs/GenRec/Holdouts13.lean
@@ -10,8 +10,47 @@ namespace Holdouts13
 def holdout_0 : PRF 1 :=
   PRF.comp (PRF.primRec (PRF.zero 1) (PRF.primRec (PRF.primRec (PRF.succ) (PRF.primRec ((PRF.proj 2 ⟨0, by decide⟩)) ((PRF.proj 4 ⟨0, by decide⟩)))) ((PRF.proj 4 ⟨1, by decide⟩)))) prf_list![PRF.succ, PRF.zero 1]
 
+def mk_args2 (a b : Nat) : Fin 2 → Nat := fun i => if i.val = 0 then a else b
+def mk_args3 (a b c : Nat) : Fin 3 → Nat := fun i => if i.val = 0 then a else if i.val = 1 then b else c
+
+def H0_h : PRF 3 := PRF.primRec (PRF.primRec (PRF.succ) (PRF.primRec ((PRF.proj 2 ⟨0, by decide⟩)) ((PRF.proj 4 ⟨0, by decide⟩)))) ((PRF.proj 4 ⟨1, by decide⟩))
+def H0_c : PRF 2 := PRF.primRec (PRF.zero 1) H0_h
+
+lemma H0_comp (x : Nat) : evalPRF holdout_0 (fun _ => x) = evalPRF H0_c (mk_args2 (x + 1) 0) := by
+  change evalPRF H0_c (fun j => evalPRFList prf_list![PRF.succ, PRF.zero 1] j (fun _ => x)) = evalPRF H0_c (mk_args2 (x + 1) 0)
+  apply congrArg (evalPRF H0_c)
+  funext ⟨val, isLt⟩
+  match val with
+  | 0 => rfl
+  | 1 => rfl
+
+lemma H0_h_base : evalPRF H0_h (mk_args3 0 1 0) = 1 := by rfl
+lemma H0_h_base_0 : evalPRF H0_h (mk_args3 0 0 0) = 1 := by rfl
+lemma H0_h_step (m acc y : Nat) : evalPRF H0_h (mk_args3 (m + 1) acc y) = evalPRF H0_h (mk_args3 m acc y) := by rfl
+
+lemma H0_h_const (m : Nat) : evalPRF H0_h (mk_args3 m 1 0) = 1 := by
+  induction m with
+  | zero => exact H0_h_base
+  | succ m ih => rw [H0_h_step]; exact ih
+
+lemma H0_c_step (x : Nat) : evalPRF H0_c (mk_args2 (x + 1) 0) = evalPRF H0_h (mk_args3 x (evalPRF H0_c (mk_args2 x 0)) 0) := by rfl
+
+lemma H0_c_val (x : Nat) : evalPRF H0_c (mk_args2 (x + 1) 0) = 1 := by
+  induction x with
+  | zero =>
+    rw [H0_c_step 0]
+    have h0 : evalPRF H0_c (mk_args2 0 0) = 0 := by rfl
+    rw [h0]
+    exact H0_h_base_0
+  | succ x ih =>
+    rw [H0_c_step (x + 1)]
+    rw [ih]
+    exact H0_h_const (x + 1)
+
 theorem holdout_0_diverges : ∀ x, evalPRF holdout_0 (fun _ => x) > 0 := by
-  sorry
+  intro x
+  rw [H0_comp x, H0_c_val x]
+  exact Nat.zero_lt_one
 
 -- Translating holdout 1
 -- M(C(R(P(1,1),R(R(P(1,1),R(P(2,1),P(4,1))),P(4,2))),S,S))
